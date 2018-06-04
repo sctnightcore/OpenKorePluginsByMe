94e41b915368c72e2ba402326c67c9bb4d22b547
Maytichai Saowa <nightcoremax@gmail.com>
2018-06-04 08:01:20 +0700

    999999999

--- OpenkoreToDiscord/Plugin/OpenkoreToDiscord.pl
+++ OpenkoreToDiscord/Plugin/OpenkoreToDiscord.pl
@@ -4,11 +4,11 @@
 # This software is open source, licensed under the GNU General Public
 # License, version 2.
 #
-# Whenever the bot recives a PM, it will forward to Discord
+# Whenever the bot recives , it will forward to Discord
 #
 # Use at your own risk.
 #
-# This plugin should be in a subfolder of plugins like 'pluginsPmtoDiscordPmtoDiscord.pl'.
+# This plugin should be in a subfolder of plugins like 'plugins/OpenkoreToDiscord/OpenkoreToDiscord.pl'.
 #
 ############################
 
@@ -17,80 +17,78 @@ package OpenkoreToDiscord;
 use strict;
 use Plugins;
 use Log qw(warning message error debug);
-use JSONTiny qw(encode_json);
-use LWPUserAgent;
+use JSON::Tiny qw(encode_json);
+use LWP::UserAgent;
 use I18N qw(bytesToString);
 use Globals;
+use Misc;
+use Time::HiRes qw(time);
+use Translation;
 
 
 
+Plugins::register('OpenkoreToDiscord', 'Forwards received to Discord.', \&onUnload);
 
-Pluginsregister('OpenkoreToDiscord', 'Forwards received to Discord.', &onUnload);
-
-my $hooks = PluginsaddHooks(
-        ['packet_privMsg', &receivedPM],
-		['disconnected',	&disconnected],
-		['self_died',	&self_died],
-		['base_level_changed',	&base_level_changed],
-		['job_level_changed',	&job_level_changed],
-		['NetworkReceivemap_changed',	&map_changed],			
+my $hooks = Plugins::addHooks(
+        ['packet_privMsg', \&receivedPM],
+		['disconnected',	\&disconnected],
+		['self_died',	\&self_died],
+		['base_level_changed',	\&base_level_changed],
+		['job_level_changed',	\&job_level_changed],
+		['Network::Receive::map_changed',	\&map_changed],			
 );
 
 sub onUnload {
-    PluginsdelHooks($hooks);
+    Plugins::delHooks($hooks);
 }
 
 
 sub disconnected {
-	my ($self, $args) = @_;
-	my $message = '```Openkore Status Changed to DISCONNECTED```';
-	message Send self_died To Discord!n;
-	discordnotifier($message);
+	my $MESSAGE = ('```Openkore Status Changed to: DISCONNECTED```');
+	debug "Send self_died To Discord!\n";
+	discordnotifier($MESSAGE);
 }
 	
 sub self_died {
-	my ($self, $args) = @_;
-	my $message = '```The '.$char-{name}.' DIED in '.$field-name.'```';
-	message Send self_died To Discord!n;
-	discordnotifier($message);
+	my $MESSAGE = ('```The '.$char->{name}.' DIED in '.$field->name.'```');
+	debug "Send self_died To Discord!\n";
+	discordnotifier($MESSAGE);
 }
 
 sub base_level_changed {
 	my ($self, $args) = @_;
-	my $message = '```The '.$char-{name}.' is now in base level '.$args-{level}.'```';
-	message Send base_level_changed To Discord!n;
-	discordnotifier($message);
+	my $MESSAGE = ('```The '.$char->{name}.' is now in base level '.$args->{level}.'```');
+	debug "Send base_level_changed To Discord!\n";
+	discordnotifier($MESSAGE);
 }
 
 sub job_level_changed {
 	my ($self, $args) = @_;
-	my $message = '```The '.$char-{name}.' is now in job level '.$args-{level}.'```';
-	message Send job_level_changed To Discord!n;
-	discordnotifier($message);
+	my $MESSAGE = ('```The '.$char->{name}.' is now in job level '.$args->{level}.'```');
+	debug "Send job_level_changed To Discord!\n";
+	discordnotifier($MESSAGE);
 }
 
 sub map_changed {
 	my ($self, $args) = @_;
-	my $message = '```The '.$char-{name}.' changed map from '.$args-{oldMap} . ' to ' . $field-name. '```';
-	message Send map_changed To Discord!n;
-	discordnotifier($message);
+	return unless ($field->name ne $args->{oldMap});
+	my $MESSAGE = ('```The '.$char->{name}.' changed map from: '.$args->{oldMap} . ' to: ' . $field->name. '```');
+	debug "Send map_changed To Discord!\n";
+	discordnotifier($MESSAGE);
 }
 
 
 
 sub discordnotifier {
-	my ($self, $user, $message) = @_;
-
-	my %content = ('username' = 'OpenKore', 'content' = $message);
-	my $json = encode_json(%content);
-	
-	require LWPUserAgent;
-	LWPUserAgent-new-post(
-	'httpsdiscordapp.comapiwebhooks452478667252039680Lqb0i4-cKm9_OClAtZI-ETFeK2BHYecZXBijggSWchjwzJlFxK820jTo_WISibWoqBBD', 
-	'Content-Type' = 'applicationjson',
-	'User-Agent' = 'Mozilla4.0',
-	'Content' = $json,
+	my ($MESSAGE) = @_;
+	my %content = ('username' => 'OpenKore', 'content' => $MESSAGE);
+	my $json = encode_json(\%content);
+	require LWP::UserAgent;
+	LWP::UserAgent->new->post(
+	'https://discordapp.com/api/webhooks/452977815445569537/zIBp-624XFPe6h3KZYNrrMsUjGsf4tSi4K2U2eyRdyXyzTtOXBuQiOnG5H_-pmb09frL', 
+	'Content-Type' => 'application/json',
+	'User-Agent' => 'Mozilla/4.0',
+	'Content' => $json,
 	);
 }
-
 1;
